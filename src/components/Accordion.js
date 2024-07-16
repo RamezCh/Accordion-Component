9c563eeb3fd06590979dd7a0e83bc38d1cf0bb11
RamezCh <ramiz_chreide@outlook.com>
2024-07-16 23:06:47 +0300

    V2 Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,36 @@
 import AccordionItem from './AccordionItem';
+import { useState } from 'react';
 
 export function Accordion({ data }) {
+  const [curOpen, setIsOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((faq, index) => (
         <AccordionItem
           key={index}
           title={faq.title}
-          text={faq.text}
           num={index}
-        />
+          curOpen={curOpen}
+          onOpen={setIsOpen}
+        >
+          {faq.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        key="Thinking in React"
+        title="Thinking in React"
+        num={22}
+        curOpen={curOpen}
+        onOpen={setIsOpen}
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into Components</li>
+          <li>Make Components reusable</li>
+          <li>Place State efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
